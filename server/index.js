dbc9802e4aced73c39f5714769ad14c7b934428e
akshitabajaj03 <akshitabajaj709@gmail.com>
2024-01-04 23:33:05 +0530

    message

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const socketio = require("socket.io");
 const http = require("http");
 const router = require("./router");
 const cors = require('cors');
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,11 +17,26 @@ const io = socketio(server, {cors: {
 app.use(cors());
 
 io.on('connection', (socket) =>{
-    console.log("We have a new connection");
+    socket.on('join', ({name,room}, callback) =>{
+        const {error, user} = addUser({id:socket.id, name, room});
 
-    socket.on('join', ({name,room}) =>{
-        console.log(name,room);
+        if(error)
+        {
+            return callback(error);
+        }
+
+        socket.emit('message', 
+        {user: "admin", text: `${user.name}, welcome to the room ${user.room}`});
+
+        socket.broadcast.to(user.room).emit('message', 
+        {user: "admin", text:`${user.name}has joined!`});
+
+        socket.join(user.room);
+
+        callback();
     })
+
+    socket.on('sendMessage', )
 
     socket.on('disconnect', ()=>{
         console.log("User left");
